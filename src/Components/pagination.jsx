972e3bc328c459f7d29f9b6c627e1b5ee8709115
Codablack23 <77158925+Codablack23@users.noreply.github.com>
2022-02-02 11:54:09 +0100

    Update pagination.jsx

--- src/Components/pagination.jsx
+++ src/Components/pagination.jsx
@@ -4,10 +4,10 @@ export default function Pagination(){
     <nav aria-label="Page navigation example">
        <ul className="pagination justify-content-end">
                <li className="page-item disabled">
-                  <a className="page-link" href="#" tabIndex="-1" aria-disabled="true"><i className="bi bi-chevron-double-left"></i></a>
+                  <a className="page-link" tabIndex="-1" aria-disabled="true"><i className="bi bi-chevron-double-left"></i></a>
                </li>
                <li className="page-item">
-               <a className="page-link" href="#"><i className="bi bi-chevron-double-right"></i></a>
+               <a className="page-link"><i className="bi bi-chevron-double-right"></i></a>
                </li>
        </ul>
     </nav>
